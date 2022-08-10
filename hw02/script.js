96219cffb3c4513bda0082b6e47eda73a183fa79
Elena <hardevich@gmail.com>
2022-08-10 18:50:12 +0300

    added hw02

--- hw02/script.js
+++ hw02/script.js
@@ -1,38 +1,56 @@
-const products = [
-  { id: 1,
-    title: 'Ball',
-    price: 500,
-    img: "img/ball.jpeg"
-  } ,
-  { id: 2,
-    title: 'Boar',
-    price: 850,
-    img: "img/boar.jpeg"
-  } ,
-  { id: 3,
-    title: 'Puller',
-    price: 300,
-    img: "img/puller.png"
-  } ,
-  { id: 4,
-    title: 'Set Toys',
-    price: 1200,
-    img: "img/set-toys.jpeg"
-  } ,
+class ProductsList {
+  constructor(container = '.products') {
+    this.container = container;
+    this.products = [];
+    this.allProducts = [];
+    this._fetchProducts();
+  }
+
+  _fetchProducts() {
+    this.products = [
+      { id: 1, title: 'Shirt', price: 150, img: "img/short.jpg" },
+      { id: 2, title: 'Socks', price: 50, img: "img/socks.jpg" },
+      { id: 3, title: 'Jacket', price: 350, img: "img/jacket.jpg" },
+      { id: 4, title: 'Shoes', price: 250, img: "img/shoes.jpg" },
   ];
+  }
 
-const renderProduct = (product) => {
-  return ` <div class="product-item">
-    <img src="${product.img}" alt="">
-    <h3>${product.title}</h3>
-    <p>${product.price}</p>
-    <button class="buy-btn">Купить</button>
-  </div>`
-};
+  render() {
+    const block = document.querySelector(this.container);
+    for (const product of this.products) {
+      const productObj = new ProductItem(product);
+      this.allProducts.push(productObj);
+      block.insertAdjacentHTML('beforeend', productObj.renderProduct())
+    }
+  }
 
-const renderPage = list => {
-  document.querySelector('.products').innerHTML = 
-    (list.map(product => renderProduct(product))).join('');
-};
+  getSum() {
+    let s = 0;
+    this.products.forEach(item => {
+      s += item.price;
+    })
+    alert(s);
+  }
+}
 
-renderPage(products);
+class ProductItem {
+  constructor(product){
+    this.title = product.title;
+    this.id = product.id;
+    this.price = product.price;
+    this.img = product.img;
+  }
+
+  renderProduct() {
+    return `<div class="product-item" data-id="${this.id}">
+              <img src="${this.img}" alt="">
+              <h3>${this.title}</h3>
+              <p>$${this.price}</p>
+              <button class="buy-btn">Buy</button>
+            </div>`
+  }
+}
+
+let list = new ProductsList();
+list.render();
+list.getSum();
